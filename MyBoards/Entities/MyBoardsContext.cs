7eed7500738267f2a87b0989633c00e668f47777
Antoni Nowalski <antoninowalski@gmail.com>
2023-09-20 14:18:48 +0200

    Konfiguracja modelu i maly refaktor

---
+++ MyBoards/Entities/MyBoardsContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyBoards.Entities
+{
+    public class MyBoardsContext : DbContext
+    {
+        public MyBoardsContext(DbContextOptions<MyBoardsContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<WorkItem> WorkItems { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<WorkItem>(eb =>
+            {
+                eb.Property(p => p.State).IsRequired();
+                eb.Property(p => p.Area).HasColumnType("varchar(200)");
+                eb.Property(p => p.IterationPath).HasColumnName("Iteration_Path");
+                eb.Property(p => p.Effort).HasColumnType("decimal(5,2)");
+                eb.Property(p => p.EndDate).HasPrecision(3);
+                eb.Property(p => p.Activity).HasMaxLength(200);
+                eb.Property(p => p.RemaningWork).HasPrecision(14, 2);
+            });
+
+        }
+
+    }
+}
--- MyBoards/Program.cs
+++ MyBoards/Program.cs
@@ -1,10 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using MyBoards.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<MyBoardsContext>(
+    option => option.UseSqlServer(builder.Configuration.GetConnectionString("MyBoardsConnectionStrings"))
+    );
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
